8053fbd701da439abba1e060da24d7fc880e1fb2
Paulo Victor <paulovictor@live.com>
2017-10-27 00:33:54 -0200

    Primeiro commit, Identify2 implementado

---
+++ src/superblock.c
@@ -0,0 +1,4 @@
+//
+// Created by Paulo Victor on 27/10/17.
+//
+
---
+++ src/teste.c
@@ -0,0 +1,26 @@
+//
+// Created by aluno on 27/10/17.
+//
+
+#include <stdio.h>
+#include "../include/t2fs.h"
+
+int main () {
+
+    printf("==== TESTE DO IDENTIFY2 ====\n\n");
+
+    char nomes[200];
+    identify2(nomes,200);
+    puts(nomes);
+
+    printf("============================\n\n");
+
+
+
+    return 0;
+
+
+
+
+
+}
